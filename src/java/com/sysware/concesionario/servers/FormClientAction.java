172d41e8b1a4478b7d9be444e891f6d4ce9cb967
andiazher <andiazhe@unal.edu.co>
2017-03-15 14:54:19 -0500

    Update FormClientAction.java

--- src/java/com/sysware/concesionario/servers/FormClientAction.java
+++ src/java/com/sysware/concesionario/servers/FormClientAction.java
@@ -64,6 +64,9 @@ public class FormClientAction extends HttpServlet {
                         int s= propietario.getColums().indexOf("CEDULA");
                         propietario.getData().set(s, identificaction);
                         propietario.getData().set(propietario.getColums().indexOf("TIPODOC"), ti);
+                        propietario.getData().set(propietario.getColums().indexOf("FNACIMIENTO"), "0000-00-00");
+                        propietario.getData().set(propietario.getColums().indexOf("CIUDAD"), "0");
+                        propietario.getData().set(propietario.getColums().indexOf("PAIS"), "0);
                         propietario.create();
                         propietario = propietario.getEntitieParam("CEDULA", identificaction).get(0);
                     }
